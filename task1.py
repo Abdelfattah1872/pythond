5fb70710c39b52dc6b53bbeb0d32937d5717d0d2
abdo200010002@gmail.com <abdo200010002@gmail.com>
2023-05-02 20:40:17 +0300

    first commit

--- task1.py
+++ task1.py
@@ -1,2 +1,11 @@
-print('Task1')
+def Replace(string, position=5, character='K'):
+    # make the text a list#
+    mytext = list(string)
+    # Replace the char as per the position#
+    mytext[position] = character
+    # Rearrange the text#
+    final = ''.join(mytext)
+    print(final)
 
+
+Replace('abracadabra')
